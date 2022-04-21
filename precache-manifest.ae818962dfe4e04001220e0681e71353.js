7f2f23b98d7e10f6784e433aff8ecb6d1d8a9b94
ricsign <ricsign123@gmail.com>
2022-04-21 15:05:40 -0600

    Updates

---
+++ precache-manifest.ae818962dfe4e04001220e0681e71353.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ae7ec0c9ebf591993ae",
+    "url": "/covid-tracker/static/css/main.e939985e.chunk.css"
+  },
+  {
+    "revision": "7ae7ec0c9ebf591993ae",
+    "url": "/covid-tracker/static/js/main.4b82eb95.chunk.js"
+  },
+  {
+    "revision": "2a53377d599573c648d2",
+    "url": "/covid-tracker/static/js/runtime~main.3339ab03.js"
+  },
+  {
+    "revision": "4fa5c6a70ec4c3f0e92b",
+    "url": "/covid-tracker/static/js/2.a8fa943d.chunk.js"
+  },
+  {
+    "revision": "9f2a05bd7163708e4768",
+    "url": "/covid-tracker/static/js/3.1d875b50.chunk.js"
+  },
+  {
+    "revision": "55647843afabb6c36705982482bbcec6",
+    "url": "/covid-tracker/static/media/title.55647843.jpeg"
+  },
+  {
+    "revision": "f1561184fd1a595662d88e41d01435d8",
+    "url": "/covid-tracker/index.html"
+  }
+];
